cbc56ad3c0698688c3a1e681ee6189d9d65a4793
João Vitor Dornellas Cypriano <69093222+joao-cypriano@users.noreply.github.com>
2024-11-11 21:02:22 +0000

    Update BFS.py

--- Graphs/BFS.py
+++ Graphs/BFS.py
@@ -1,19 +1,3 @@
-"""pseudo-code"""
-
-"""
-BFS(graph G, start vertex s):
-// all nodes initially unexplored
-mark s as explored
-let Q = queue data structure, initialized with s
-while Q is non-empty:
-    remove the first node of Q, call it v
-    for each edge(v, w):  // for w in graph[v]
-        if w unexplored:
-            mark w as explored
-            add w to Q (at the end)
-
-"""
-
 import collections
 
 
